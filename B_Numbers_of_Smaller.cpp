e2a3d17be21e3cdd4f40b664eae412c99c1f1db1
Sumonaroy444 <154045972+Sumonaroy444@users.noreply.github.com>
2024-11-11 23:07:41 +0600

    Add files via upload
    week3 updated

---
+++ B_Numbers_of_Smaller.cpp
@@ -0,0 +1,25 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int>a(n),b(m);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+     for(int i=0;i<m;i++){
+        cin>>b[i];
+    }
+    int l=0,r=0,cnt =0;
+    while(r<m){
+        if(l<n && a[l]<b[r]){
+            cnt++,l++;
+        }
+        else{
+            cout<<cnt<<" ";
+            r++;
+        }
+    }
+    return 0;
+}
